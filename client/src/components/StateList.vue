64c249b601e788923e0ab300e7dadf6980e77de8
David <nacevskidavid@gmail.com>
2021-05-13 19:28:56 -0500

    50 states app

---
+++ client/src/components/StateList.vue
@@ -0,0 +1,74 @@
+<template>
+
+
+  <div>
+    <state-summary v-bind:states="states"></state-summary>
+  </div>
+  <div class="state-list-container">
+    <div class="state-container" v-for="state in states" v-bind:key="state.name">
+      <state-detail
+          v-bind:state="state"
+          v-on:update-visited="updateVisited"
+
+      ></state-detail>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+import StateDetail from "@/components/StateDetail";
+import StateSummary from "@/components/StateSummary";
+
+export default {
+  name:'StateList',
+  components: {
+    StateDetail,
+    StateSummary
+  },
+  data(){
+    return {
+      states:[]
+    }
+  },
+  mounted() {
+    this.fetchAllStates()
+  },
+  methods:{
+    fetchAllStates(){
+        this.$stateService.getAllStates().then( states=>{
+          this.states = states
+        })
+      .catch(err => {
+        alert('Sorry, can\'t fetch state list')
+        console.error(err)
+      })
+    },
+    updateVisited(stateName,visited){
+      this.$stateService.setVisited(stateName,visited).then( () => {
+        this.fetchAllStates()
+      })
+      .catch(err =>{
+        alert('Sorry can\'t update state')
+        console.error(err)
+      })
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+.state-list-container{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+
+.state-container{
+  margin: 1rem;
+}
+
+</style>
---
+++ routes/states.js
@@ -0,0 +1,46 @@
+let express = require('express')
+let States = require('../models').States
+
+let router = express.Router()
+
+
+// fetch all states
+router.get('/states',function (req,res,next){
+    States.findAll({order:['name']}).then(states=>{
+        return res.json(states)
+    })
+        .catch(err => next(err))
+})
+
+
+router.get('/state/:name', function (req,res,name){
+    let stateName = req.params.name
+    States.findOne( {where: {name:stateName}})
+        .then( state =>{
+            if (state){
+                return res.json(state)
+            }
+            else {
+                return res.status(404).send('State not found')
+            }
+        })
+        .catch(err =>next(err))
+})
+
+//patch route to update state status
+router.patch('/states/:name',function (req,res,next){
+    let stateName = req.params.name
+    let stateVisited =req.body.visited
+
+    States.update({ visited: stateVisited}, {where: {name:stateName}})
+        .then(rowsUpdated => {
+            let numberOfRowsUpdated = rowsUpdated[0]
+            if(numberOfRowsUpdated==1){
+                return res.send('ok')
+            }
+            return res.status(404).send('State not found')
+        })
+        .catch(err => next(err))
+})
+
+module.exports = router
